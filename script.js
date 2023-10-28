2bdbd016c558e4ada0706cd676d1e44e43df5313
0xPew <0xPew@proton.me>
2023-10-28 23:50:16 +0800

    Add roleValue

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const submitButton = document.getElementById("submit");
+const roleInput = document.getElementById("role-input");
 const textInput = document.getElementById("text-input");
 const responseContainer = document.getElementById("response");
 
@@ -14,32 +15,24 @@ const stopLoadingAnimation = () => {
   submitButton.style.cursor = "pointer";
 };
 
-const showResponse = (chosenCard, content) => {
+const showResponse = (content) => {
   responseContainer.style.display = "block";
-  responseContainer.textContent = "Card category: " + chosenCard + "\n\n" + content;
+  responseContainer.textContent = content;
 };
 
 submitButton.addEventListener("click", () => {
+  startLoadingAnimation(); // Start loading animation when the button is clicked
+
+  const roleValue = roleInput.value;
   const inputValue = textInput.value;
-
-  const cards = [
-    "The Fool", "The Magician", "The High Priestess", "The Empress", "The Emperor",
-    "The Hierophant", "The Lovers", "The Chariot", "Strength", "The Hermit",
-    "Wheel of Fortune", "Justice", "The Hanged Man", "Death", "Temperance",
-    "The Devil", "The Tower", "The Star", "The Moon", "The Sun", "Judgement", "The World"
-  ];
-
-  const chooseRandomCard = () => cards[Math.floor(Math.random() * cards.length)];
-  const chosenCard = chooseRandomCard();
 
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
 
   const raw = JSON.stringify({
     "input": {
-      "question": inputValue,
-      "name": chosenCard,
-      "json_only": "format the result in JSON Object"
+      "request": inputValue,
+      "job_role": roleValue
     }
   });
 
@@ -50,16 +43,16 @@ submitButton.addEventListener("click", () => {
     redirect: 'follow'
   };
 
-  fetch("https://pms.chasm.net/api/prompts/execute/31", requestOptions)
+  fetch("https://pms.chasm.net/api/prompts/execute/220", requestOptions)
     .then(response => response.json())
     .then(result => {
-      const content = JSON.parse(result.content).quatrain;
-      stopLoadingAnimation();
-      showResponse(chosenCard, content);
+      const content = result.content;
+      stopLoadingAnimation(); // Stop loading animation when the response is received
+      showResponse(content);
     })
     .catch(error => {
       console.log('error', error);
-      stopLoadingAnimation();
+      stopLoadingAnimation(); // Stop loading animation in case of an error
       submitButton.textContent = "Error";
     });
 });
