ad5e0510b982bd5baf1dee24c835542d80a31d52
0xPew <0xPew@proton.me>
2023-11-06 17:24:58 +0800

    Implement 2nd API call and response

--- emo_support/script.js
+++ emo_support/script.js
@@ -8,22 +8,37 @@ const question1Container = document.getElementById("question1-container");
 const question2Container = document.getElementById("question2-container");
 const question3Container = document.getElementById("question3-container");
 const question4Container = document.getElementById("question4-container");
+const answer1Input = document.getElementById("answer1-input");
+const answer2Input = document.getElementById("answer2-input");
+const answer3Input = document.getElementById("answer3-input");
 const answer1Button = document.getElementById("answer1-send-button");
 const answer2Button = document.getElementById("answer2-send-button");
 const answer3Button = document.getElementById("answer3-send-button");
 const answer4Button = document.getElementById("answer4-send-button");
+const choice = document.getElementById("choice");
 
-
-const startLoadingAnimation = () => {
-  submitButton.disabled = true;
-  submitButton.innerHTML = '<i class="fas fa-spinner fa-spin"></i>';
-  submitButton.style.cursor = "not-allowed";
+const startLoadingAnimation = (button) => {
+  if (button === "submitButton") {
+    submitButton.disabled = true;
+    submitButton.innerHTML = '<i class="fas fa-spinner fa-spin"></i>';
+    submitButton.style.cursor = "not-allowed";
+  } else if (button === "answer4Button") {
+    answer4Button.disabled = true;
+    answer4Button.innerHTML = '<i class="fas fa-spinner fa-spin"></i>';
+    answer4Button.style.cursor = "not-allowed";
+  }
 };
 
-const stopLoadingAnimation = () => {
-  submitButton.disabled = false;
-  submitButton.innerHTML = '<i class="fas fa-check"></i>';
-  submitButton.style.cursor = "pointer";
+const stopLoadingAnimation = (button) => {
+  if (button === "submitButton") {
+    submitButton.disabled = false;
+    submitButton.innerHTML = '<i class="fas fa-check"></i>';
+    submitButton.style.cursor = "pointer";
+  } else if (button === "answer4Button") {
+    answer4Button.disabled = false;
+    answer4Button.innerHTML = '<i class="fas fa-check"></i>';
+    answer4Button.style.cursor = "pointer";
+  }
 };
 
 const showResponse = (content) => {
@@ -51,11 +66,53 @@ answer3Button.addEventListener("click", () => {
   question4Container.style.display = "block";
 });
 
+answer4Button.addEventListener("click", () => {
+  startLoadingAnimation("answer4Button");
+
+  const inputValue = textInput.value;
+  const choiceValue = choice.value;
+  const questionsValue = "1. " + question1.innerHTML + " 2. " + question2.innerHTML + " 3. " + question3.innerHTML;
+  const responseValue = "1. " + answer1Input.value + " 2. " + answer2Input.value + " 3. " + answer3Input.value;
+
+  const myHeaders = new Headers();
+
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({
+    input: {
+      worries: inputValue,
+      questions: questionsValue,
+      response: responseValue,
+      choice: choiceValue
+    }
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+
+  fetch("https://pms.chasm.net/api/prompts/execute/222", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      const content = result.content;
+      showResponse(content);
+      stopLoadingAnimation("answer4Button");
+    })
+    .catch(error => {
+      stopLoadingAnimation("answer4Button");
+      responseContainer.innerHTML = "<h2 class='list-heading'>Error, please try again later</h2>";
+    });
+});
+
 submitButton.addEventListener("click", () => {
-  startLoadingAnimation();
+  startLoadingAnimation("submitButton");
   question1Container.style.display = "none";
   question2Container.style.display = "none";
   question3Container.style.display = "none";
+  question4Container.style.display = "none";
   const inputValue = textInput.value;
   const myHeaders = new Headers();
 
@@ -79,11 +136,10 @@ submitButton.addEventListener("click", () => {
     .then(result => {
       const content = result.content;
       displayQuestions(content);
-      // showResponse(content);
-      stopLoadingAnimation();
+      stopLoadingAnimation("submitButton");
     })
     .catch(error => {
-      stopLoadingAnimation();
+      stopLoadingAnimation("submitButton");
       responseContainer.innerHTML = "<h2 class='list-heading'>Error, please try again later</h2>";
     });
 });
